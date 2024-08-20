f58ef57d5fdd5f621c2e14400e159a800906fb20
soryeon kim <kimsr9210@naver.com>
2024-08-20 12:19:42 +0900

    싱글 파일 컴포넌트

---
+++ vue-cli/src/a.vue
@@ -0,0 +1,27 @@
+let appHeader = {
+    template:'<div>header</div>',
+    methods:{
+        addNum:function(){
+
+        }
+    }
+}
+        
+<template>
+    <div>header</div>
+</template>
+
+<script>
+export default {
+    //js - 인스턴스 옵션
+    methods:{
+        addNum:function(){
+
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
