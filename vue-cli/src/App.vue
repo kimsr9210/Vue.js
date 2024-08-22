9adfd6541a9798fc841ba7e54f555d82acaa15c2
soryeon kim <kimsr9210@naver.com>
2024-08-22 14:32:15 +0900

    싱글 파일 컴포넌트에 배운 내용 적

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,32 +1,19 @@
 <template>
   <div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <!--컴포넌트 명명법 종류-->
-    <!-- <hello-world></hello-world>
-    <helloWorld></helloWorld> -->
-    <OtherComponent/>
+    {{str}}
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  //인스턴스 옵션 속성 or 컴포넌트 옵션 속성
-  name: 'App',
-  components: {
-    HelloWorld
+  data: function(){
+    return{
+      str:'hi'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
