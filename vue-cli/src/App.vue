1477877f89b79ff362b91e9306f40db8d0b9a037
soryeon kim <kimsr9210@naver.com>
2024-08-23 13:11:13 +0900

    싱글 파일 컴포넌트 체계에서 컴포넌트 등록

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-    {{str}}
+    <app-header></app-header>
   </div>
 </template>
 
 <script>
+import AppHeader from './components/AppHeader.vue' //파일의 내용을 변수에 담음
+
+//기존 방법
+// let AppHeader = {
+//   template:'<header><h1>Header</h1></header>'
+// }
+
 export default {
   data: function(){
     return{
       str:'hi'
     }
+  },
+  components:{
+    'app-header':AppHeader
   }
 }
 </script>
