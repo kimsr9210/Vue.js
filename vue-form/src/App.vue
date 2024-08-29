301606e728761cf9c00b9813183923ec224bd887
soryeon kim <kimsr9210@naver.com>
2024-08-29 13:44:56 +0900

    프로젝트 생성 및 마크업 작업

---
+++ vue-form/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="">
+    <div>
+      <label from="username">id : </label>
+      <input id="username" type="text">
+    </div>
+    <div>
+      <label from="password">pw : </label>
+      <input id="password" type="password">
+    </div>
+    <button>login</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
