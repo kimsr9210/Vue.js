2a4152101e0f7b8737c54737034b3dfa35254e20
soryeon kim <kimsr9210@naver.com>
2024-09-13 16:57:54 +0900

    App.vue

--- vue-intermediate-main/vue-todo/src/App.vue
+++ vue-intermediate-main/vue-todo/src/App.vue
@@ -9,37 +9,34 @@
 
 <script>
 import TodoHeader from './components/TodoHeader.vue'
-import TodoFooter from './components/TodoFooter.vue'
-import TodoInput from './components/TodoInput.vue'
+import TodoInput from './components/TodoIInput.vue'
 import TodoList from './components/TodoList.vue'
+import TodoFooter from './components/TodoFooter.vue'
 
-export default {
-  components:{
-    //컴포넌트 태그명 : 컴포넌트 내용
-    'TodoHeader' : TodoHeader,
-    'TodoFooter' : TodoFooter,
-    'TodoInput' : TodoInput,
-    'TodoList' : TodoList
-  }
+export default {  
+ components: {
+   //컴포넌트 태그명: 컴포넌트 내용
+   'TodoHeader': TodoHeader,
+   'TodoInput': TodoInput,
+   'TodoList': TodoList,
+   'TodoFooter': TodoFooter
+ } 
 }
 </script>
 
 <style>
-body{
+body {
   text-align: center;
-  background-color: #f6f6f6;
+  background-color: #F6F6F6;
 }
-
-input{
+input {
   border-style: groove;
   width: 200px;
 }
-
-button{
-   border-style: groove;
+button {
+  border-style: groove;
 }
-
-.shadow{
+.shadow {
   box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
 }
 </style>
