817a741f6933b011aec24cf9548323dbd65eab43
soryeon kim <kimsr9210@naver.com>
2024-09-26 15:30:16 +0900

    [리팩토링]할 일 완료 기능

--- vue-intermediate-main/vue-todo/src/App.vue
+++ vue-intermediate-main/vue-todo/src/App.vue
@@ -4,7 +4,9 @@
     <!-- <TodoInput v-on:하위 컴포넌트에서 발생시킨 이벤트 이름="현재 컴포넌트의 메서드 명"></TodoInput> -->
     <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
     <!-- <TodoList v-bind:내려보낼 프롭스 속성 이름="현재 위치의 컴포넌트 데이터 속성"></TodoList> -->
-    <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem"></TodoList>
+    <TodoList v-bind:propsdata="todoItems" 
+      v-on:removeItem="removeOneItem" 
+      v-on:toggleItem="toggleOneItem"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -30,6 +32,11 @@ export default {
     removeOneItem:function(todoItem,index){
       localStorage.removeItem(todoItem.item);
 			this.todoItems.splice(index, 1);
+    },
+    toggleOneItem: function(todoItem, index){
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+			localStorage.removeItem(todoItem.item);
+			localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   },
   created: function() {
