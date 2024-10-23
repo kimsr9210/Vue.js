36647f115655784e9270f733e1d592f7f5e9c8a6
kimsoryeon <kimsr9210@naver.com>
2024-10-23 14:35:53 +0900

    Vuex 설치

--- vue-intermediate-main/vue-todo/src/main.js
+++ vue-intermediate-main/vue-todo/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {store} from './store/store'
 import '@fortawesome/fontawesome-free/css/all.css'; // Font Awesome CSS 추가
 
 Vue.config.productionTip = false
 
 new Vue({
+  store,
   render: h => h(App),
 }).$mount('#app')
---
+++ vue-intermediate-main/vue-todo/src/store/store.js
@@ -0,0 +1,8 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    // 여기에 상태(state), 변이(mutations), 행동(actions) 등을 정의할 수 있습니다.
+});
