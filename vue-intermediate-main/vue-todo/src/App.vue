e27ccf795aca2618ef2ff7abecfc1ff1ffa77641
kimsoryeon <kimsr9210@naver.com>
2024-10-24 17:02:10 +0900

    [리팩토링]state 속성 적용

--- vue-intermediate-main/vue-todo/src/App.vue
+++ vue-intermediate-main/vue-todo/src/App.vue
@@ -43,15 +43,6 @@ export default {
       this.todoItems = [];
     }
   },
-  created() {
-		if (localStorage.length > 0) {
-			for (let i = 0; i < localStorage.length; i++) {
-				if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-					this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-				}
-			}
-		}
-	},
   components:{
     //컴포넌트 태그명 : 컴포넌트 내용
     //'TodoList' : TodoList
--- vue-intermediate-main/vue-todo/src/store/store.js
+++ vue-intermediate-main/vue-todo/src/store/store.js
@@ -3,6 +3,22 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const storage = {
+    fetch(){
+        const arr = [];
+        if (localStorage.length > 0) {
+            for (let i = 0; i < localStorage.length; i++) {
+                if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
+                    arr.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+                }
+            }
+        }
+        return arr;
+    },
+}
+
 export const store = new Vuex.Store({
-    // 여기에 상태(state), 변이(mutations), 행동(actions) 등을 정의할 수 있습니다.
+    state: {
+        todoItems: storage.fetch()
+    }
 });
