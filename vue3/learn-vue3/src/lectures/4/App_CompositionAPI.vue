21869933b214085614847156c40939171fd80005
kimsoryeon <kimsr9210@naver.com>
2024-12-27 14:57:02 +0900

    Composition API

---
+++ vue3/learn-vue3/src/lectures/4/App_CompositionAPI.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <h2>반응형 메시지</h2>
+    <p>{{ reactiveMessage }}</p>
+    <button v-on:click="addReactiveMessage">Add Message</button>
+    <h2>일반 메세지</h2>
+    <p>{{ normalMessage }}</p>
+    <button v-on:click="addNormalMessage">Add Message</button>
+  </div>
+</template>
+
+<script>
+import { isRef ,onBeforeMount,onMounted,ref } from 'vue'; //ref : 반응형 api
+
+  export default {
+    setup(){
+      console.log('setup()');
+      const reactiveMessage = ref('Hello Reactive Message');
+      const addReactiveMessage = () => {
+        reactiveMessage.value = reactiveMessage.value + '!';
+      }
+
+      console.log('ifRef(reactiveMessage): ' , isRef(reactiveMessage));
+
+      let normalMessage = 'Hello Normal Message';
+      const addNormalMessage = () => {
+        normalMessage = normalMessage + '!';
+      }
+
+      console.log('ifRef(normalMessage): ' , isRef(normalMessage));
+
+      onMounted(() => {
+        console.log('onMounted');
+      });
+
+      onBeforeMount(() => {
+        console.log('onBeforeMount')
+      });
+
+      return{
+        reactiveMessage,
+        normalMessage,
+        addReactiveMessage,
+        addNormalMessage
+      };
+    },
+  };
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue3/learn-vue3/src/main.js
+++ vue3/learn-vue3/src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from './lectures/4/App_CompositionAPI.vue'
 
 createApp(App).mount('#app')
