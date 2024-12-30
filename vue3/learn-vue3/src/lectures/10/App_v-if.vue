2d79bb9a32267d2b607737b796dd96648ac3c89f
kimsoryeon <kimsr9210@naver.com>
2024-12-30 15:05:53 +0900

    v-if

---
+++ vue3/learn-vue3/src/lectures/10/App_v-if.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h2 v-if="visible">Hello Vue3</h2>
+        <h2 v-else>flase 입니다.</h2>
+        <button v-on:click="visible = !visble">else로 글씨 바꾸기</button>
+        <hr />
+        <button v-on:click="type = 'A'">A</button>
+        <button v-on:click="type = 'B'">B</button>
+        <button v-on:click="type = 'C'">C</button>
+        <button v-on:click="type = 'D'">D</button>
+
+        <h2 v-if="type === 'A'">A입니다.</h2>
+        <h2 v-else-if="type === 'B'">B입니다.</h2>
+        <h2 v-else-if="type === 'C'">C입니다.</h2>
+        <h2 v-else>A, B ,C가 아닙니다.</h2>
+        <hr />
+        <template v-if="visible">
+            <h1>Title</h1>
+            <p>Paragraph 1</p>
+            <p>Paragraph 2</p>
+        </template>
+        <hr />
+        <h1 v-show="showOk">Title 입니다.</h1>
+        <button v-on:click="showOk = !showOk">show toggle</button>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup () {
+        const visible = ref(false);
+        const type = ref('A');
+        const showOk = ref(true);
+        return {visible, type, showOk}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- vue3/learn-vue3/src/main.js
+++ vue3/learn-vue3/src/main.js
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './lectures/App_style_binding.vue'
+import App from './lectures/App_v-if.vue'
 
 const app = createApp(App);
 app.config.devtools = true;
