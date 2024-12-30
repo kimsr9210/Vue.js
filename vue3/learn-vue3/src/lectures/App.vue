9a69a127ba118be85ffd9bbda2fc69e8981cdacb
kimsoryeon <kimsr9210@naver.com>
2024-12-30 14:05:00 +0900

    ./assets 삭제

---
+++ vue3/learn-vue3/src/lectures/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div :class="{ active: isActive}">텍스트 입니다.</div>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue';
+export default {
+    setup () {
+        const isActive = ref(true);
+
+        return {
+            isActive,
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+.active {
+    font-weight:900;
+}
+</style>
