4bc668a57b74a7f393d8866b43bddfb989c33d7c
kimsoryeon <kimsr9210@naver.com>
2024-12-31 13:56:42 +0900

    event

---
+++ vue3/learn-vue3/src/lectures/13/App.event.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <button @click="printEventInfo('Hello Vue3', $event)">
+            인라인 이벤트 핸들러
+        </button>
+        <hr />
+        <input @keyup="onKeyupHandler"/>
+    </div>
+</template>
+
+<script>
+export default {
+    setup () {
+        const printEventInfo = (message, event) => {
+            console.log('messsage: ', message);
+            console.log(event.target);
+            console.log(event.target.tagName);
+        }
+        const onKeyupHandler = event => {
+            console.log('event.key:' , event.key)
+        }
+
+        return {
+            printEventInfo, onKeyupHandler
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
